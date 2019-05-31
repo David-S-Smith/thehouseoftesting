b9d4424fcc4d612f5e6f59793845465187f43680
David-S-Smith <yinyangbell@gmail.com>
2019-05-31 09:51:39 -0700

    hopefully fixed bot crashing off local

--- commands/hilo.js
+++ commands/hilo.js
@@ -1,5 +1,6 @@
 module.exports = msg => {
     var random_boolean = Math.random() >= 0.5;
+    Console.log("hilo happened");
     if(random_boolean){
         msg.reply("***High.*** :arrow_up:");
     }
